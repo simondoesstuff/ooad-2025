7a7536cbed445e645289d8b508f89212623427eb
Simon Walker <simon@simonwalker.tech>
2025-06-07 18:25:44 +0000

    project setup

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "A basic Java development flake with Gradle";
+
+  inputs = {
+    nixpkgs.url = "github:Nixos/nixpkgs/nixos-25.05";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gradle
+          ];
+        };
+      }
+    );
+}
