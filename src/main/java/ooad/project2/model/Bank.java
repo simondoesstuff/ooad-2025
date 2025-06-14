2b208fb1b2f89df2717bde7b7ba9d973ea2b7ec5
Simon Walker <simon@simonwalker.tech>
2025-06-14 03:19:30 +0000

    proj2: Bank singleton

---
+++ src/main/java/ooad/project2/model/Bank.java
@@ -0,0 +1,30 @@
+package ooad.project2.model;
+
+/**
+ * singleton to track total money withdrawn from the bank.
+ */
+public class Bank {
+    private static Bank instance;
+    private double totalWithdrawn;
+
+    private Bank() {
+        this.totalWithdrawn = 0.0;
+    }
+
+    public static Bank getInstance() {
+        if (instance == null) {
+            instance = new Bank();
+        }
+        return instance;
+    }
+
+    public void withdraw(double amount) {
+        if (amount > 0) {
+            this.totalWithdrawn += amount;
+        }
+    }
+
+    public double getTotalWithdrawn() {
+        return totalWithdrawn;
+    }
+}
